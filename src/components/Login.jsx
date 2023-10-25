d3d9e211b444ff8a2e9bc3ccb400d289dbd7966f
Matias Ariel Eguia <matias_eguia@hotmail.com>
2023-10-24 21:19:08 -0300

    retocando algunos estilos y conectando con la api para usuarios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,24 @@ import logo from '../assets/logo.png'
 import back from '../assets/back.png'
 import Footer from './Footer';
 import NavSecun from './NavSecun';
+const URL='https://647683b09233e82dd53a1337.mockapi.io/'
 
 function Login() {
+    const handleSubmitSignIn=async(e)=>{
+      e.preventDefault();
+      const inputUserLogin=document.getElementById('username-login').value;
+      const inputPassLogin=document.getElementById('password-login').value;
+      let notification=document.getElementsByClassName('.notification').innerText;
+      const res = await fetch(URL+'/users')
+      const parsed = await res.json()
+      const auth= parsed.find(usr => usr.user_name === inputUserLogin && usr.password === inputPassLogin )
+      auth? notification='Session iniciada': notification='Usuario y/o contraseña erroneo/s';
+    }
+    const handleSubmitSignUp=(e)=>{
+      e.preventDefault();
+      console.log('Creando usuario');
+    }
+
     return (
     <>
       <NavSecun/>
@@ -12,33 +28,25 @@ function Login() {
         <div className='border-form'>
         <div className='cont-ini cont-iniB'>
           <h2>Iniciar Sesión</h2>
-          <form  className='formLogin' >
+          <form  className='formLogin' onSubmit={handleSubmitSignIn} >
             <div>
               <label className="labelLogin" htmlFor="username-login">Usuario</label>
-              <input
-              className='inputLogin'
-                type="text"
-                id="username-login"
-              
-              />
+              <input className='inputLogin' type="text" id="username-login"/>
             </div>
             <div>
               <label className="labelLogin" htmlFor="password-login">Contraseña</label>
-              <input
-              className='inputLogin'
-                type="password"
-                id="password-login"
-              
-            
-              />
+              <input className='inputLogin' type="password" id="password-login"/>
             </div>
+            <div>
+              <p className="notification">sdfsdfsdf</p>
             <button className='btnLogin' type="submit">Ingresar</button>
+            </div>
           </form>
         </div>
 
         <div className='cont-ini'>
           <h2>Crear Cuenta</h2>
-          <form className='formLogin'>
+          <form className='formLogin' onSubmit={handleSubmitSignUp}>
             <div>
               <label className="labelLogin" htmlFor="username-signup">Usuario</label>
               <input
